7a86f22df6ba7d6833932eda3f533cc80a062e15
Jesse Rodrigues <104332254+Jrodrygues@users.noreply.github.com>
2022-05-13 22:26:47 -0300

    ajuste depoimentos

--- main.js
+++ main.js
@@ -87,3 +87,19 @@ ScrollReveal({
   #about, 
   #about header, 
   #about .content`)  
+
+  /* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
